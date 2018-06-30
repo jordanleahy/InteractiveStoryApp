5445d45d443eda18745e38441ccbc673a0e5c78c
Jordan Leahy <jordanwilliamleahy@gmail.com>
2018-06-30 14:16:31 -0400

    Creating artworkView Progammatically
    Add func viewWillLayoutoutSubviews()
    Add in body NSLayoutConstraint.activate to set constraints

--- InterActiveStoryApp/PageController.swift
+++ InterActiveStoryApp/PageController.swift
@@ -19,6 +19,13 @@ class PageController: UIViewController {
     //Add stored property to hold the page we'll be working with.
     var page: Page?
     
+    // MARK: - User Interface Properties
+    
+    let artworkView = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .system)
+    let secondChoiceButton = UIButton(type: .system)
+    
     
     //Call superclass initializer because init coder is a designated initializer.  Without, we'd get 'required initializer 'init(c0der:)' must be provided by sublcass of 'UIViewcontroller' Error. 
     required init?(coder aDecoder: NSCoder) {
@@ -36,13 +43,33 @@ class PageController: UIViewController {
         
         //Unwrap since page is an optional property. 
         if let page = page {
-            print(page.story.text)
+            
+            artworkView.image = page.story.artwork  //Shows first image/artwork inside of an image view for the first page in our story
         }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    ///Creating Views Progammatically
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        //Add artworkView as a subview of our main view.  Same as dragging UIImageView in IB
+        view.addSubview(artworkView)
+        //Turn off iOS's automatic constraint view so we can add our own constraints
+        artworkView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        ///Set constraints by pinning artwork anchors to subview to main view anchors
+        NSLayoutConstraint.activate([
+            artworkView.topAnchor.constraint(equalTo: view.topAnchor),
+            artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            artworkView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            artworkView.leftAnchor.constraint(equalTo: view.leftAnchor)
+            ])
     }
     
 
