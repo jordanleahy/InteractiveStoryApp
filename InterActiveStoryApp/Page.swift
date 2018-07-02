a2be02a4d482ecca4d04d6424f07f602e04d254c
Jordan Leahy <jordanwilliamleahy@gmail.com>
2018-07-02 14:34:25 -0400

    viewDidLoad Updates
    Create attributedString w/ linespacing
    Add two buttons hat display Page buttons

--- InterActiveStoryApp/Page.swift
+++ InterActiveStoryApp/Page.swift
@@ -26,13 +26,15 @@ class Page {
 
 extension Page {
     
+    //MARK: - Helper Methodss
+    
     /// Inside the body of this method, we'll use the story that we're passing in to create a page, and initialize it with a story and then simply call through to func addChoiceWith(title: String, page: Page) -> Page to do all the work.
     func addChoiceWith(title: String, story: Story) -> Page {
         let page = Page(story: story)
         return addChoiceWith(title: title, page: page)
     }
     
-    ///Method to take title and page as a parameter and add choice to a Page instance.  A switch statement can switch on multiple values by compounding them into a tuple.
+    ///Method to take title and page as a parameter and add Choice to a page instance.  A switch statement can switch on multiple values by compounding them into a tuple.
     func addChoiceWith(title: String, page: Page) -> Page {
         switch (firstChoice, secondChoice) {
         case (.some, .some): break // or we could put return self instead of break
--- InterActiveStoryApp/PageController.swift
+++ InterActiveStoryApp/PageController.swift
@@ -11,7 +11,7 @@
 
 */
 
-
+import Foundation
 import UIKit
 
 class PageController: UIViewController {
@@ -27,12 +27,12 @@ class PageController: UIViewController {
     let secondChoiceButton = UIButton(type: .system)
     
     
-    //Call superclass initializer because init coder is a designated initializer.  Without, we'd get 'required initializer 'init(c0der:)' must be provided by sublcass of 'UIViewcontroller' Error. 
+    //Call superclass initializer because init coder is a designated initializer.  Without, we'd get 'required initializer 'init(c0der:)' must be provided by sublcass of 'UIViewcontroller' Error.  We need init coder since we are loading a ViewController throught storyboard.
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    //Call appropriate designated initialer on UIViewController to initialize up the chain and pass in nil, nil
+    //This is our subclass designated initializer. Call appropriate designated initialer on UIViewController to initialize up the chain and pass in nil, nil
     init(page: Page) {
         self.page = page
         super.init(nibName: nil, bundle: nil)
@@ -41,10 +41,34 @@ class PageController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         //Unwrap since page is an optional property. 
         if let page = page {
             
             artworkView.image = page.story.artwork  //Shows first image/artwork inside of an image view for the first page in our story
+            
+            //Contains the string we want to display along with a dictionary with certain keys to make modifications
+            let attributedString = NSMutableAttributedString(string: page.story.text)
+          
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 10
+            
+            attributedString.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+            
+            storyLabel.attributedText = attributedString
+            
+            // See if firstChoice is nil or not. Set button text to firstChoice text. If firstChoice nil, "Play Again"
+            if let firstChoice = page.firstChoice {
+                firstChoiceButton.setTitle(firstChoice.title, for: .normal)
+            } else {
+                firstChoiceButton.setTitle("Play Again", for: .normal)
+            }
+            
+            if let secondChoice = page.secondChoice {
+                secondChoiceButton.setTitle(secondChoice.title, for: .normal)
+            }
+            
+            
         }
     }
 
@@ -53,7 +77,8 @@ class PageController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    ///Creating Views Progammatically
+    //MARK: - Creating Views Progammatically
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
@@ -63,12 +88,44 @@ class PageController: UIViewController {
         artworkView.translatesAutoresizingMaskIntoConstraints = false
         
         
-        ///Set constraints by pinning artwork anchors to subview to main view anchors
+        //Set constraints by pinning artworkView anchors to main view anchors
         NSLayoutConstraint.activate([
             artworkView.topAnchor.constraint(equalTo: view.topAnchor),
             artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             artworkView.rightAnchor.constraint(equalTo: view.rightAnchor),
             artworkView.leftAnchor.constraint(equalTo: view.leftAnchor)
+            ])
+        
+        //Add storyLabel as a subview in our main view.
+        view.addSubview(storyLabel)
+        //Allows swift to determine how many lines it needs to display to show all the text. Default is 1 line
+        storyLabel.numberOfLines = 0
+        
+        //Turn off iOS's automatic constraint view so we can add our own constraints
+        storyLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            storyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0), // 16 points
+            storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            storyLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -48.0)
+            
+            ])
+        //Add firstChoiceButton as a subviw in our main view
+        view.addSubview(firstChoiceButton)
+        firstChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            firstChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0)
+            ])
+        //Add secondChoiceButton as a subviw in our main view
+        view.addSubview(secondChoiceButton)
+        secondChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            secondChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32)
             ])
     }
     
